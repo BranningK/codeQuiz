9bdc042a541af990d02021cc348fe5d39ce68984
Branning <bmk2110@gmail.com>
2022-12-17 15:02:21 -0500

    updated homepage

---
+++ assets/script.js
@@ -0,0 +1,16 @@
+function setTime() {
+    // Sets interval in variable
+    var timerInterval = setInterval(function() {
+      secondsLeft--;
+      timeEl.textContent = secondsLeft + " seconds left till colorsplosion.";
+  
+      if(secondsLeft === 0) {
+        // Stops execution of action at set interval
+        clearInterval(timerInterval);
+        // Calls function to create and append image
+        sendMessage();
+      }
+  
+    }, 1000);
+  }
+  
