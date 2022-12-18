81d6e36ab4998da508ab0806f3f3b0823aa8870f
BranningK <bmk2110@gmail.com>
2022-12-18 02:21:33 -0500

    replaced links with divs

--- assets/script.js
+++ assets/script.js
@@ -9,10 +9,10 @@
 //Question text
     var question = document.querySelector('#question');
 //Answer choice boxes
-    var choiceBox1 = document.querySelector('#answer1').style.display = 'none';
-    var choiceBox2 = document.querySelector('#answer2').style.display = 'none';
-    var choiceBox3 = document.querySelector('#answer3').style.display = 'none';
-    var choiceBox4 = document.querySelector('#answer4').style.display = 'none';
+    var answerChoice1 = document.querySelector('#answerChoice1').style.display = 'none';
+    var answerChoice2 = document.querySelector('#answerChoice2').style.display = 'none';
+    var answerChoice3 = document.querySelector('#answerChoice3').style.display = 'none';
+    var answerChoice4 = document.querySelector('#answerChoice4').style.display = 'none';
 //Answer choice text
     var answerChoice1 = document.querySelector('#answerChoice1');
     var answerChoice2 = document.querySelector('#answerChoice2');
@@ -23,7 +23,6 @@
 
 //Adds event listener to start button and ties it to the beginQuiz() function
     startButton.addEventListener("click", beginQuiz);
-
 //Starts quiz and begins the timer
     function beginQuiz(event) {
         console.log('Starting Quiz');
@@ -62,37 +61,51 @@
 function startQuiz(){
     //Displays question 1
         showQuestion1();
+        console.log('Question 1 displaying');
         showChoices();
+        console.log('Showing the question answer choices');  
     //Checks if answer is correct or not
             if(answer = true) {
-                console.log("Correct +5 points! ");
+                console.log("Correct! ");
             } else {
                 console.log("Incorrect -2 seconds ")
+                secondsLeft -2;
             }
 
 
     //Shows question 1
         function showQuestion1(){
     //Shows the question's text
-            console.log('Question 1 displaying');
             question.textContent = "Example question text"
         }
     //Shows the answer boxes
-            console.log('Showing the question answer choices');  
         function showChoices(){
             console.log('Showing answer choices');
-            choiceBox1 = document.querySelector('#answer1').style.display = 'flex';
-            choiceBox2 = document.querySelector('#answer2').style.display = 'flex';
-            choiceBox3 = document.querySelector('#answer3').style.display = 'flex';
-            choiceBox4 = document.querySelector('#answer4').style.display = 'flex';
+            answerChoice1 = document.querySelector('#answerChoice1').style.display = 'flex';
+            answerChoice2 = document.querySelector('#answerChoice2').style.display = 'flex';
+            answerChoice3 = document.querySelector('#answerChoice3').style.display = 'flex';
+            answerChoice4 = document.querySelector('#answerChoice4').style.display = 'flex';
     //Shows the user the answer choices
-            answerChoice1.textContent = "asdf";
-            answerChoice2.textContent = "asdf";
-            answerChoice3.textContent = "asdf";
-            answerChoice4.textContent = "asdf";
+            text1.textContent = "asdf";
+            text2.textContent = "asdf";
+            text3.textContent = "asdf";
+            text4.textContent = "asdf";
+    //Event listeners for the answer boxes
+            answerChoice1.addEventListener("click", function(event){
+                event.stopPropogation();
+
+                console.log("yuhhhh")
+            });
+            // text2.addEventListener("click", function(event));
+            // text3.addEventListener("click", function(event));
+            // text4.addEventListener("click", function(event));
+            
+
+            
     //Ideas for answer verification
             //Contains (answer)
             //Answer = Boolean
+            //if <answerChoice> correct, else wrong -- maybe best bet?
         }
 
 
