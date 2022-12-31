11447a1c5dc547cb919ba6590cc572f029bf35af
Branning <bmk2110@gmail.com>
2022-12-31 18:26:36 -0500

    Fixed question one answer accepting all

--- assets/script.js
+++ assets/script.js
@@ -3,16 +3,16 @@
 //Start button
     var startButton = document.querySelector('#startButton');
 //Time display
-    var timeLeft = document.querySelector('#timeLeft').style.display = 'none';
+    document.querySelector('#timeLeft').style.display = 'none';
 //Question box
     var questionBox = document.querySelector('#questionBox');
 //Question text
     var question = document.querySelector('#question');
 //Answer choice boxes
-    var answerChoice1 = document.querySelector('#answerChoice1').style.display = 'none';
-    var answerChoice2 = document.querySelector('#answerChoice2').style.display = 'none';
-    var answerChoice3 = document.querySelector('#answerChoice3').style.display = 'none';
-    var answerChoice4 = document.querySelector('#answerChoice4').style.display = 'none';
+    document.querySelector('#answerChoice1').style.display = 'none';
+    document.querySelector('#answerChoice2').style.display = 'none';
+    document.querySelector('#answerChoice3').style.display = 'none';
+    document.querySelector('#answerChoice4').style.display = 'none';
 //Answer choice text
     var answerChoice1 = document.querySelector('#answerChoice1');
     var answerChoice2 = document.querySelector('#answerChoice2');
@@ -35,79 +35,83 @@
         event.preventDefault();
     }
 
-//Hides the start button
-    function hideStartBtn(){
-        startButton.style.display = 'none';
-        console.log("Start button hidden"); 
 
-    }
-//Starts timer
-    function startTimer() {
-        console.log(secondsLeft + " seconds left.");
-        document.querySelector('#timeLeft').style.display = 'flex';
-
-        var timerInterval = setInterval(function() {
-            secondsLeft--;
-            document.querySelector('#timeLeft').textContent = secondsLeft + " seconds left.";
-        
-            if(secondsLeft === 0) {
-            // Stops execution of action at set interval
-            clearInterval(timerInterval);
-            // Calls function to create and append image
-            sendMessage();
-            }
-        
-        }, 1000);
-
-    }
 //Starts quiz
 function startQuiz(){
     //Displays question 1
-        showQuestion1();
+        question1();
         console.log('Question 1 displaying');
-        showChoices();
-        console.log('Showing the question answer choices');  
+        // showChoices();
+        // console.log('Showing the question answer choices');  
     //Checks if answer is correct or not
-            if(answer = true) {
-                console.log("Correct! ");
-            } else {
-                console.log("Incorrect -2 seconds ")
-                secondsLeft -2;
-            }
+        // checkAnswer();
+        // console.log('Checking Answer');
 
 
     //Shows question 1
-        function showQuestion1(){
+        function question1(){
+            showChoices();
     //Shows the question's text
-            question.textContent = "Example question text"
+            question.textContent = "Example question text" 
+            // answer = answerChoice3
+
+            //Shows the answer boxes
+                function showChoices(){
+                    console.log('Showing answer choices');
+                    document.querySelector('#answerChoice1').style.display = 'flex';
+                    document.querySelector('#answerChoice2').style.display = 'flex';
+                    document.querySelector('#answerChoice3').style.display = 'flex';
+                    document.querySelector('#answerChoice4').style.display = 'flex';
+            //Shows the user the answer choices
+                    text1.textContent = "1. AAAA";
+                    text2.textContent = "2. BBBB";
+                    text3.textContent = "3. CCCC";
+                    text4.textContent = "4. DDDD";
+            //Event listeners for the answer boxes
+                    answerChoice1.addEventListener("click", checkAnswer1);
+                    answerChoice2.addEventListener("click", checkAnswer1);
+                    answerChoice3.addEventListener("click", checkAnswer1);
+                    answerChoice4.addEventListener("click", checkAnswer1);
+                }
         }
-    //Shows the answer boxes
-        function showChoices(){
-            console.log('Showing answer choices');
-            answerChoice1 = document.querySelector('#answerChoice1').style.display = 'flex';
-            answerChoice2 = document.querySelector('#answerChoice2').style.display = 'flex';
-            answerChoice3 = document.querySelector('#answerChoice3').style.display = 'flex';
-            answerChoice4 = document.querySelector('#answerChoice4').style.display = 'flex';
-    //Shows the user the answer choices
-            text1.textContent = "asdf";
-            text2.textContent = "asdf";
-            text3.textContent = "asdf";
-            text4.textContent = "asdf";
+}
+//Need to add a points storage object in the local storage!!!!!!
+
+function checkAnswer1(event) {
+    if(event.target !== answerChoice3) {
+        console.log("Incorrect -2 seconds ");
+        secondsLeft --;
+    } else {
+        console.log("Correct! ");
+        //insert points to local storage
+    }
+}
+
+// var answer1 = answerChoice3
+//List of functions
+
+//Hides the start button
+function hideStartBtn(){
+    startButton.style.display = 'none';
+    console.log("Start button hidden"); 
+}
+//Starts timer
+function startTimer() {
+    console.log(secondsLeft + " seconds left.");
+    document.querySelector('#timeLeft').style.display = 'flex';
+
+    var timerInterval = setInterval(function() {
+        secondsLeft--;
+        document.querySelector('#timeLeft').textContent = secondsLeft + " seconds left.";
+    
+        if(secondsLeft === 0) {
+        // Stops execution of action at set interval
+        clearInterval(timerInterval);
+        // Calls function to create and append image
+        console.log('Time is up');
         }
-    //Event listeners for the answer boxes
-            answerChoice1.addEventListener("click", console.log('eventListener'));
-            // text2.addEventListener("click", function(event));
-            // text3.addEventListener("click", function(event));
-            // text4.addEventListener("click", function(event));
-            
 
-            
-    //Ideas for answer verification
-            //Contains (answer)
-            //Answer = Boolean
-            //if <answerChoice> correct, else wrong -- maybe best bet?
-        
-
-
+    }, 1000);
 
 }
+
